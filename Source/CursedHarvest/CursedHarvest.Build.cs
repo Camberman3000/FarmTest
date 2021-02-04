09a8222577afa6b099384f592e3f3d13c95bd8f2
Camberman3000 <camberman3000@hotmail.com>
2021-02-04 06:34:34 -0500

    updated to current

--- Source/CursedHarvest/CursedHarvest.Build.cs
+++ Source/CursedHarvest/CursedHarvest.Build.cs
@@ -8,6 +8,8 @@ public class CursedHarvest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "AIModule" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "AIModule" });
 	}
 }
